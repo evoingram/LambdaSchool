d604812c6aaafcd2f1e1d1608a28f12e963f8895
Erica L. Ingram <inquiries@aquoco.co>
2019-11-08 03:01:31 -0800

    13a1 challenge 1-2 done

--- m1/13a1/assignments/objects.js
+++ m1/13a1/assignments/objects.js
@@ -3,11 +3,6 @@
 // ==== Challenge 1: Writing Objects ==== 
 // HR needs some information on the new interns put into a database.  Given an id, email, first name, and gender. Create an object for each person in the company list:
 
-// 1, mmelloy0@psu.edu, Mitzi, F
-// 2, kdiben1@tinypic.com, Kennan, M
-// 3, kmummery2@wikimedia.org, Keven, M
-// 4, gmartinson3@illinois.edu, Gannie, M
-// 5, adaine5@samsung.com, Antonietta, F
 
 // Example format of an intern object: 1, examples@you.edu, Example, F
 const example = {
@@ -19,19 +14,62 @@ const example = {
 
 // Write your intern objects here:
 
+const internMitzi = {
+  id: 1,
+  name: "Mitzi",
+  email: "mmelloy0@psu.edu",
+  gender: "F",
+}
 
+const internKennan = {
+  id: 2,
+  name: "Kennan",
+  email: "kdiben1@tinypic.com",
+  gender: "M",
+}
+
+const internKeven = {
+  id: 3,
+  name: "Keven",
+  email: "kmummery2@wikimedia.org",
+  gender: "M",
+}
+
+const internGannie = {
+  id: 4,
+  name: "Gannie",
+  email: "gmartinson3@illinois.edu",
+  gender: "M",
+}
+
+const internAntonietta = {
+  id: 5,
+  name: "Antonietta",
+  email: "adaine5@samsung.com",
+  gender: "F",
+}
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
+// 1, mmelloy0@psu.edu, Mitzi, F
+console.log(internMitzi.name);
 
+// 2, kdiben1@tinypic.com, Kennan, M
 // Kennan's ID
+console.log(internKennan.id);
 
+// 3, kmummery2@wikimedia.org, Keven, M
 // Keven's email
+console.log(internKeven.email);
 
+// 4, gmartinson3@illinois.edu, Gannie, M
 // Gannie's name
+console.log(internGannie.name);
 
+// 5, adaine5@samsung.com, Antonietta, F
 // Antonietta's Gender
+console.log(internAntonietta.gender);
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
