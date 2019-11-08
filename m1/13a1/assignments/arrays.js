5ae5d7ea034280abfb3a84538d8695369ef5a6e3
Erica L. Ingram <inquiries@aquoco.co>
2019-11-08 04:11:42 -0800

    13a1 obj chall 1-3 done + array chal 1-5 done

--- m1/13a1/assignments/arrays.js
+++ m1/13a1/assignments/arrays.js
@@ -94,14 +94,27 @@ console.log(carModelsSorted);
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
 let carYears = [];
-console.log();
+for (let x = 0; x < inventory.length; x++) {
+  carYears[x] = inventory[x].car_year;
+}
+console.log(carYears);
 
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
 let oldCars = [];
-console.log();
+console.log(carYears[0]);
+for (let x = 0; x < carYears.length; x++) {
+  if (carYears[x] <= 1999) {
+    oldCars.push(carYears[x]);
+  }
+}
+console.log(oldCars.length);
 
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory. Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
 let BMWAndAudi = [];
 console.log();
+function newFunction() {
+  return 2000;
+}
+
