eb2bd7eb32a4ec41e498d77ea8dae20703273c15
Erica L. Ingram <inquiries@aquoco.co>
2019-11-08 03:42:24 -0800

    13a1 objects chal 1-3 + arrays chal1 done

--- m1/13a1/assignments/arrays.js
+++ m1/13a1/assignments/arrays.js
@@ -1,4 +1,4 @@
-rea// To help us use arrays with real world problems we are going to simulate a used car dealer that has 50 cars in their inventory.
+// To help us use arrays with real world problems we are going to simulate a used car dealer that has 50 cars in their inventory.
 
 // The car dealer has all of their inventory housed in the array seen below.  Scroll down past the data to find out how you can help the car dealer.
 
@@ -75,7 +75,7 @@ let inventory = [
 
 // ==== Challenge 1 ====
 // The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by logging the car's year, make, and model in the console log provided to you below:
-console.log(`Car 33 is a *car year goes here* *car make goes here* *car model goes here*`);
+console.log("Car 33 is a " + inventory[33].car_year + " " + inventory[33].car_make + " " + inventory[33].car_model + "."); 
 
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
