bd8f2cab7c1825af2178cbae0da1d770b85156df
Erica L. Ingram <inquiries@aquoco.co>
2019-11-14 14:53:21 -0800

    13d1 project done, not able to do testing

--- m1/13d1/new/index.js
+++ m1/13d1/new/index.js
@@ -40,8 +40,27 @@ class Airplane {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-class Person {
 
+
+class Person {
+  constructor(name, age, someFood) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+  eat(someFood) {
+    if (this.stomach.length < 10) {
+      this.stomach.push(someFood);
+    } else {
+      this.stomach = [];
+    }
+  }
+  poop() {
+    this.stomach = [];
+  }
+  toString() {
+    return this.name + ", " + this.age;
+  }
 }
 
 /*
@@ -59,7 +78,29 @@ class Person {
 */
 
 class Car {
-
+  constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons) {
+    return `(${this.tank} += ${gallons})`;
+  }
+  drive(distance) {
+    let gallonsRequired = distance / this.milesPerGallon;
+    let possibleDistance = this.tank * this.milesPerGallon;
+    if (this.tank > gallonsRequired) {
+      this.odometer += distance;
+      this.tank -= gallonsRequired;
+    } else if (this.tank < gallonsRequired && this.tank > 0) {
+      this.odometer += possibleDistance;
+      this.tank -= this.tank;
+      return `I ran out of fuel at ${possibleDistance} miles!`;
+    } else {
+      return `I ran out of fuel at ${this.odometer} miles!`;
+    }
+  }
 }
 
 /*
@@ -75,7 +116,15 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
+  constructor(name, age, location) {
+    this.name = name;
+    this.age = age;
+    this.location = location;
+  }
 
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
 }
 
 /*
@@ -92,8 +141,21 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
-
+class Instructor extends Lambdasian {
+  constructor(name, age, location, specialty, favLanguage, catchPhrase) {
+    super(name, age, location);
+    this.specialty = specialty;
+    this.favLanguage = favLanguage;
+    this.catchphrase = catchPhrase;
+  }
+  demo(subject) {
+    return `Today we are learning about ${subject}`;
+    // receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
+  }
+  grade(student, subject) {
+    return `${student.name} receives a perfect score on ${subject}.`;
+    //receives a student object and a subject string as arguments and logs out '{student.name} receives a perfect score on {subject}'
+  }
 }
 
 /*
@@ -111,8 +173,39 @@ class Instructor {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
+class Student extends Lambdasian {
+  constructor(name, age, location, previousBackground, className, favSubjects, grade) {
+    super(name, age, location);
+    this.previousBackground = previousBackground;
+    this.className = className;
+    this.favSubjects = favSubjects;
+    this.grade = grade;
+  // stretch: Extend the functionality of the Student by adding a prop called grade and setting it equal to a number between 1-100.
+  }
 
+  listsSubjects() {
+    return `Loving ${this.favSubjects}`;
+    // ` a method that returns all of the student's favSubjects in a single string: `Loving HTML, CSS, JS!`.
+  }
+  PRAssignment(subject) {
+    return `${this.name} has submitted a PR for ${subject}`;
+    // a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
+  }
+  sprintChallenge(subject) {
+    return `${this.name} has submitted sprint challenge on ${subject}`;
+    //similar to PRAssignment but returns student.name has begun sprint challenge on {subject}
+  }
+  randomGrade() {
+    this.grade = Math.round((Math.random()) * 100);
+    // stretch: Now that our students have a grade build out a method on the Instructor (this will be used by BOTH instructors and PM's) that will randomly add or subtract points to a student's grade. Math.random will help.
+  }
+  graduate() {
+    if (this.grade >= 70) {
+      return `${this.name} is ready to graduate!`
+    }
+    // stretch: Add a graduate method to a student.  This method, when called, will check the grade of the student and see if they're ready to graduate from Lambda School.  If the student's grade is above a 70% let them graduate! Otherwise go back to grading their assignments to increase their score.
+
+  }
 }
 
 /*
@@ -128,9 +221,32 @@ class Student {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
+class ProjectManager extends Instructor {
+  constructor(
+    name,
+    age,
+    location,
+    specialty,
+    favLanguage,
+    catchPhrase,
+    gradClassName,
+    favInstructor
+  ) {
+    super(name, age, location, specialty, favLanguage, catchPhrase);
+    this.gradClassName = gradClassName;
+    this.favInstructor = favInstructor;
+  }
 
+  standUp(slackChannel) {
+    return `${this.name} announces to ${slackChannel} @channel standy times!`;
+    // a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
+  }
+  debugsCode(student, subject) {
+    return `${this.name} debugs ${student.name}'s code on ${subject}.`;
+    //  a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
+  }
 }
+
 
 /*
   STRETCH PROBLEM (no tests!)
