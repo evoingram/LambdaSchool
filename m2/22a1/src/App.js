0e78a271799a0ecb4a9646ecedee8361adbbea08
Erica L. Ingram <inquiries@aquoco.co>
2019-11-30 06:26:27 -0800

    22a1 step 3 & 4 done

--- m2/22a1/src/App.js
+++ m2/22a1/src/App.js
@@ -5,23 +5,26 @@ import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+  const teamH = "Lions";
+  const teamA = "Tigers";
   const [scoreH, increment] = useState(0);
   const [scoreA, increment] = useState(0);
+
   return (
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
-            <h2 className="home__name">Lions</h2>
+            <h2 className="home__name">{teamH}</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score">32</div>
+            <div className="home__score">{scoreH}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <h2 className="away__name">{teamA}</h2>
+            <div className="away__score">{scoreA}</div>
           </div>
         </div>
         <BottomRow />
@@ -29,12 +32,12 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button className="homeButtons__touchdown" onClick={() => increment(scoreH + 6)}>Home Touchdown</button>
+          <button className="homeButtons__fieldGoal" onClick={() => increment(scoreH + 3)}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick={() => increment(scoreA + 6)}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick={() => increment(scoreA + 3)}>Away Field Goal</button>
         </div>
       </section>
     </div>
