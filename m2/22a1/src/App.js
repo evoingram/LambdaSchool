751b2409b175352d5e0a4d3fc327ee29c714335b
Erica L. Ingram <inquiries@aquoco.co>
2019-11-30 08:39:33 -0800

    step 5 & 8 stretch 22a1 done

--- m2/22a1/src/App.js
+++ m2/22a1/src/App.js
@@ -1,15 +1,17 @@
 //TODO: STEP 1 - Import the useState hook.
 import React, { useState } from "react";
 import "./App.css";
-import BottomRow from "./BottomRow";
-import ButtonsRow from "./ButtonsRow";
+// import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const teamH = "Lions";
   const teamA = "Tigers";
+  const touchdown = 7;
+  const fieldGoal = 3;
   const [scoreH, incrementH] = useState(0);
   const [scoreA, incrementA] = useState(0);
+  let [quarter, incrementQ] = useState(0);
   const TopRow = () => {
     return (
       <div className="topRow">
@@ -28,6 +30,7 @@ function App() {
       </div>
     );
   };
+
   const ButtonsRow = () => {
     return (
       <section className="buttons">
@@ -35,13 +38,13 @@ function App() {
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
           <button
             className="homeButtons__touchdown"
-            onClick={() => incrementH(scoreH + 7)}
+            onClick={() => incrementH(scoreH + touchdown)}
           >
             Home Touchdown
           </button>
           <button
             className="homeButtons__fieldGoal"
-            onClick={() => incrementH(scoreH + 3)}
+            onClick={() => incrementH(scoreH + fieldGoal)}
           >
             Home Field Goal
           </button>
@@ -49,21 +52,58 @@ function App() {
         <div className="awayButtons">
           <button
             className="awayButtons__touchdown"
-            onClick={() => incrementA(scoreA + 7)}
+            onClick={() => incrementA(scoreA + touchdown)}
           >
             Away Touchdown
           </button>
           <button
             className="awayButtons__fieldGoal"
-            onClick={() => incrementA(scoreA + 3)}
+            onClick={() => incrementA(scoreA + fieldGoal)}
           >
             Away Field Goal
+          </button>
+        </div>
+        <div className="buttons">
+          <button
+            className="buttons"
+            onClick={() =>
+              incrementQ(() => {
+                if (quarter === 4 || quarter === 0) {
+                  return (quarter = 1);
+                } else {
+                  return (quarter += 1);
+                }
+              })
+            }
+          >
+            Next Quarter
           </button>
         </div>
       </section>
     );
   };
-
+  const BottomRow = () => {
+    return (
+      <div className="bottomRow">
+        <div className="down">
+          <h3 className="down__title">Down</h3>
+          <div className="down__value">3</div>
+        </div>
+        <div className="toGo">
+          <h3 className="toGo__title">To Go</h3>
+          <div className="toGo__value">7</div>
+        </div>
+        <div className="ballOn">
+          <h3 className="ballOn__title">Ball on</h3>
+          <div className="ballOn__value">21</div>
+        </div>
+        <div className="quarter">
+          <h3 className="quarter__title">Quarter</h3>
+          <div className="quarter__value">{quarter}</div>
+        </div>
+      </div>
+    );
+  };
   return (
     <div className="container">
       <section className="scoreboard">
@@ -74,18 +114,30 @@ function App() {
     </div>
   );
 }
+/*
+function teamHandler(team, increment) {
+  if (team === this.teamH) {
+    return (this.scoreH += increment);
+  }
+  if (team === this.teamA) {
+    return (this.scoreA += increment);
+  }
+}*/
 
 export default App;
 
 /* TODO STEP 5 - Break out parts of this component into smaller components and compose multiple components together to make the UI */
+
 // done
 
 /* TODO STEP 6 - Write a "handler" function in the component that takes in a team name and an amount. This function will then be passed to each button's click handler. It will increment the correct team's score by the passed in amount */
+
+// could not get this to work, for some reason asking for semicolon though that's not the problem.
 
 /* TODO STEP 7 - Play around with the styling and make this project your own! Maybe make it a soccer (non-american football) scoreboard, or a rugby or baseball scoreboard */
 
 /* TODO STEP 8 - Add a button that changes which quarter the game is in, and then render the state quarter value on the scoreboard */
 
-/* TODO STEP 9 - Make the entire board fully functional with buttons and state! (If you want to make a timer, you'll have to look into useEffect) */
+// done 
 
-/* TODO STEP 10 - */
+/* TODO STEP 9 - Make the entire board fully functional with buttons and state! (If you want to make a timer, you'll have to look into useEffect) */
