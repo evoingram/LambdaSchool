75db093211061ad6e893a333ec568a02a798346d
Erica L. Ingram <inquiries@aquoco.co>
2019-12-13 08:22:09 -0800

    step 1 & 2 done

--- m2/22e1/starwars/src/App.js
+++ m2/22e1/starwars/src/App.js
@@ -9,6 +9,45 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  // styled components
+
+
+  // function app
+  function App() {
+
+    // variables to hold state
+    const [imgURL, setImgURL] = useState("");
+    const [copyright, setCopyright] = useState("");
+    const [date, setDate] = useState("");
+    const [explanation, setExplanation] = useState("");
+    const [imgURL1, setImgURL1] = useState("");
+    const [copyright1, setCopyright1] = useState("");
+    const [date1, setDate1] = useState("");
+    const [explanation1, setExplanation1] = useState("");
+
+
+
+    useEffect(
+      () => {
+
+        axios
+          .get('https://swapi.co/api/people/')
+          .then(
+            response =>{setImgURL(response.data.hdurl);
+                        setCopyright(response.data.copyright);
+                        setDate(response.data.date);
+                        setExplanation(response.data.explanation);
+            
+            }
+          )
+          .catch(error => { console.log(error); });
+        
+        
+
+      }, []);
+
+  }
+
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
