aff2a6e36317830ca244b580193eaa64550b745b
Erica L. Ingram <inquiries@aquoco.co>
2019-12-18 01:18:33 -0800

    step 4 done

--- m2/23d1/src/App.js
+++ m2/23d1/src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SavedTMList from './DisplayForm'
+import FormikForm from './Form'
 
 function App() {
+
+
+  const [users, setUsers] = useState([]);
+
+  console.log(users);
+
+  const addUser = user => {
+    setUsers( [...users, user] );
+  };
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <FormikForm component={FormikForm} addUser={addUser}/>
+      <SavedTMList list={users} />
     </div>
   );
 }
--- m2/23d1/src/Form.js
+++ m2/23d1/src/Form.js
@@ -1,10 +1,15 @@
 import React, { useState } from 'react';
-import React from "react";
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 
-function LoginForm({ errors, touched }) {
+function LoginForm({ values, errors, touched, isSubmitting }) {
+    const [users, setUsers] = useState([]);
+
+    const addUser = user => {
+        setUsers( [...users, user] );
+    };
+        
   return (
       <Form>
           {touched.name && errors.name && <p>{errors.name}</p>}
@@ -20,55 +25,57 @@ function LoginForm({ errors, touched }) {
   );
 }
 
-const Form = withFormik({
-  mapPropsToValues({ name, email, password, termsofservice }) {
-    return {
-      name: name || "",
-      email: email || "",
-      password: password || "",
-      tos: tos || false
-    };
-    },
-    // validation schema
-    validationSchema: Yup.object().shape({
-        name: Yup.string()
-            .min(5, "Name not valid")
-            .required("Name is required"),
-        email: Yup.string()
-            .email("Email not valid")
-            .required("Email is required"),
-        password: Yup.string()
-            .min(6, "Password must be 6 characters or longer")
-            .required("Password is required"),
-        tos: Yup.bool()
-            .test(
-                'tos',
-                'You have to agree with our Terms of Service!',
-                value => value === true
-            )
-            .required(
-                'You have to agree with our Terms of Service!'
-            ),
-  }),
+const FormikForm = withFormik({
+    
+    mapPropsToValues({ name, email, password, tos }) {
+        return {
+        name: name || "",
+        email: email || "",
+        password: password || "",
+        tos: tos || false
+        };
+        },
+        // validation schema
+        validationSchema: Yup.object().shape({
+            name: Yup.string()
+                .min(5, "Name not valid")
+                .required("Name is required"),
+            email: Yup.string()
+                .email("Email not valid")
+                .required("Email is required"),
+            password: Yup.string()
+                .min(6, "Password must be 6 characters or longer")
+                .required("Password is required"),
+            tos: Yup.bool()
+                .test(
+                    'tos',
+                    'You have to agree with our Terms of Service!',
+                    value => value === true
+                )
+                .required(
+                    'You have to agree with our Terms of Service!'
+                ),
+    }),
 
-handleSubmit(values, { resetForm, setErrors, setSubmitting }) {
-    if (values.email === "waffle@syrup.com") {
-      setErrors({ email: "That email is already taken" });
-    } else {
-      axios
-        .post("https://reqres.in/", values)
-        .then(res => {
-          console.log(res); // Data was created successfully and logs to console
-          resetForm();
-          setSubmitting(false);
-        })
-        .catch(err => {
-          console.log(err); // There was an error creating the data and logs to console
-          setSubmitting(false);
-        });
+    handleSubmit(values, { resetForm, setErrors, setSubmitting }) {
+        if (values.email === "waffle@syrup.com") {
+        setErrors({ email: "That email is already taken" });
+        } else {
+        axios
+            .post("https://reqres.in/", values)
+            .then(res => {
+            console.log(res); // Data was created successfully and logs to console
+            resetForm();
+                setSubmitting(false);
+                // set state for response to send to other component
+            })
+            .catch(err => {
+            console.log(err); // There was an error creating the data and logs to console
+            setSubmitting(false);
+            });
+        }
     }
-  }
 
 })(LoginForm);
 
-export default Form;
+export default FormikForm;
