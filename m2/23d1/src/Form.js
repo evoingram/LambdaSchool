7932b482d13ca3c1eca37b994056e8c7a84b5118
Erica L. Ingram <inquiries@aquoco.co>
2019-12-18 00:33:36 -0800

    step 1 done

--- m2/23d1/src/Form.js
+++ m2/23d1/src/Form.js
@@ -1,8 +1,33 @@
 import React, { useState } from 'react';
+import React from "react";
+import { withFormik, Form, Field } from "formik";
 // import TeamMembersData from './teamMembersData'
 
-const Form = props => {
-    
+function LoginForm() {
+  return (
+    <Form>
+      <Field type="text" name="name" placeholder="Name" />
+      <Field type="email" name="email" placeholder="Email" />
+      <Field type="password" name="password" placeholder="Password" />
+      <Field type="checkbox" name="termsofservice" placeholder="false"/>
+      <button>Submit!</button>
+    </Form>
+  );
 }
+
+const Form = withFormik({
+  mapPropsToValues({ name, email, password, termsofservice }) {
+    return {
+      name: name || "",
+      email: email || "",
+      password: password || "",
+      termsofservice: termsofservice || ""
+    };
+  },
+handleSubmit(values) {
+    console.log(values);
+    //THIS IS WHERE YOU DO YOUR FORM SUBMISSION CODE... HTTP REQUESTS, ETC.
+  }
+})(LoginForm);
 
 export default Form;
