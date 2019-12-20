4e8229b190d45a987776f564069a7f9ce70eaa2e
Erica L. Ingram <inquiries@aquoco.co>
2019-12-20 09:53:39 -0800

    step 1 + 2 done

--- m2/23e1/src/components/CharacterCard.js
+++ m2/23e1/src/components/CharacterCard.js
@@ -1,5 +1,26 @@
 import React from "react";
+import styled from 'styled-components'
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+const Div = styled.div`
+  background: #002244;
+  border-radius: 3px;
+  border: 2px solid #69BE28;
+  color: #A5ACAF;
+  font-weight: bold;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+`
+
+const CharacterCard = character => {
+  console.log(character);
+  return (
+    <div className="save-wrapper">
+    <div className="movie-card">
+      <h2>{character.character.name}</h2>
+      <div className="movie-director">Species: <em>{character.character.species}</em></div>
+      <div className="movie-metascore">Alive: <strong>{character.character.status}</strong></div>
+      </div>
+    </div> 
+  );
 }
+export default CharacterCard;
--- m2/23e1/src/components/CharacterList.js
+++ m2/23e1/src/components/CharacterList.js
@@ -1,16 +1,42 @@
 import React, { useEffect, useState } from "react";
+import CharacterCard from '../components/CharacterCard.js';
+import axios from 'axios';
+import { Route } from 'react-router-dom';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [character, setChar] = useState();
+  const  [characters, setCharacters] = useState([])
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    const getChars = () => {
+      axios
+        .get('https://rickandmortyapi.com/api/character/')
+        .then(response => {
+          console.log(response.data.results);
+          // setChar(response.data.results);
+          setCharacters(response.data.results);
+        })
+        .catch(error => {
+          console.error('Server Error', error);
+        });
+    }
+    
+    getChars();
   }, []);
+
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {
+        characters.map(
+          character => (
+            <CharacterCard key={character.id} character={character} />
+          )
+        )
+      }
     </section>
   );
-}
+};
