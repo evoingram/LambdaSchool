f52b77371cec57a147d7a6ea4f93865fdf3c2fa9
Erica L. Ingram <inquiries@aquoco.co>
2019-12-20 08:55:38 -0800

    initial project upload

---
+++ m2/23e1/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "./components/Header.js";
+
+
+export default function App() {
+  return (
+    <main>
+      <Header />
+    </main>
+  );
+}
---
+++ m2/23e1/src/components/CharacterCard.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CharacterCard() {
+  return <span>todo: character</span>;
+}
