c9682e02eff0cbc377c6342633a708ff687bee15
Erica L. Ingram <inquiries@aquoco.co>
2020-01-11 14:48:34 -0800

    todolist linked to data list

--- m3/31a1/src/components/TodoComponents/TodoList.js
+++ m3/31a1/src/components/TodoComponents/TodoList.js
@@ -1,11 +1,20 @@
-
 import React from 'react';
+import Data from '../../Data.js';
 
 class TodoForm extends React.Component {
   render() {
     return (
       <div>
         <h2>Todo List Will Go Here</h2>
+        <ul>
+            {
+                Data.data.map(
+                todoItem => (
+                    <li>{todoItem.task} </li>
+                )
+                )
+            }
+        </ul>
       </div>
     );
   }
