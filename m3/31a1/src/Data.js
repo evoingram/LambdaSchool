7ec811068cfa692e018c9358cdc64dabfc389ba7
Erica L. Ingram <inquiries@aquoco.co>
2020-01-11 14:15:47 -0800

    data added + app.js done

---
+++ m3/31a1/src/Data.js
@@ -0,0 +1,17 @@
+
+{
+    "data": {
+        [
+            {
+                task: 'Organize Garage',
+                id: 1528817077286,
+                completed: false
+            },
+            {
+                task: 'Bake Cookies',
+                id: 1528817084358,
+                completed: false
+            }
+        ]
+    }
+};
