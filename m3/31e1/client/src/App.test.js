a34695339f72207d8db47d54820abdada12e1a78
Erica L. Ingram <inquiries@aquoco.co>
2020-01-21 03:19:02 -0800

    initial project setup

--- m3/31e1/client/src/App.test.js
+++ m3/31e1/client/src/App.test.js
@@ -5,12 +5,12 @@ import PlayerCard from './components/PlayerCard.js';
 
 test('renders NavBar without crashing', () => {
 	const { getByText } = document.getElementById('NavBar');
-	const linkElement2 = getByText(/Soccer Players Ranked by Number of Google Searches:/i);
+	const linkElement2 = getByText(/Soccer Players Ranked by Number of Google Searches/i);
 	expect(linkElement2).toBeInTheDocument();
 });
 
 test('renders Data without crashing', () => {
 	const { getByText } = document.getElementById('PlayerCard100');
-	const linkElement2 = getByText(/Tierna Davidson:/i);
+	const linkElement2 = getByText(/Tierna Davidson/i);
 	expect(linkElement2).toBeInTheDocument();
 });
