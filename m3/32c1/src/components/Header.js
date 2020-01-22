c0657393699c984877b5609d48c9695970caa1e5
Erica L. Ingram <inquiries@aquoco.co>
2020-01-22 13:11:41 -0800

    debugging still

--- m3/32c1/src/components/Header.js
+++ m3/32c1/src/components/Header.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-class Header extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {};
-	}
-
-	render() {
-		return (
-			<>
-				<figure className="image is-128x128">
-					<img src={this.props.car.image} alt={this.props.car.name} />
-				</figure>
-				<h2>{this.props.car.name}</h2>
-				<p>Amount: ${this.props.car.price}</p>
-			</>
-		);
-	}
-}
+const Header = props => {
+	return (
+		<>
+			<figure className="image is-128x128">
+				<img src={props.car.image} alt={props.car.name} />
+			</figure>
+			<h2>{props.car.name}</h2>
+			<p>Amount: ${props.car.price}</p>
+		</>
+	);
+};
 const mapStateToProps = state => {
 	console.log(state);
 	return {
--- m3/32c1/src/reducers/reducer.js
+++ m3/32c1/src/reducers/reducer.js
@@ -1,6 +1,23 @@
 import { REMOVE_FEATURE, BUY_ITEM } from '../actions';
 
-export const reducer = (state, action) => {
+export const initialState = {
+	additionalPrice: 0,
+	car: {
+		price: 26395,
+		name: '2019 Ford Mustang',
+		image: 'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+		features: []
+	},
+	additionalFeatures: [
+		{ id: 1, name: 'V-6 engine', price: 1500 },
+		{ id: 2, name: 'Racing detail package', price: 1500 },
+		{ id: 3, name: 'Premium sound system', price: 500 },
+		{ id: 4, name: 'Rear spoiler', price: 250 }
+	]
+};
+
+export const reducer = (initialState, action) => {
+	let state = initialState;
 	switch (action.type) {
 		case BUY_ITEM:
 			if (state.car.features.includes(action.payload)) {
