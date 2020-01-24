0a51368d81a11dd92352e7171ba743a1204814ad
Erica L. Ingram <inquiries@aquoco.co>
2020-01-24 08:30:39 -0800

    app set-up middleware etc added

--- m3/32e1/smurfs/src/index.js
+++ m3/32e1/smurfs/src/index.js
@@ -1,6 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { reducer } from './reducers';
+
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	rootElement
+);
