2788e76b09891e5da02e757f197a320255163578
Erica L. Ingram <inquiries@aquoco.co>
2020-01-28 15:15:18 -0800

    app styled, mvp + 1.5 stretch met

--- m3/33b1/friends/src/components/ContactForm.js
+++ m3/33b1/friends/src/components/ContactForm.js
@@ -6,6 +6,55 @@ import axios from 'axios';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
 import styled from 'styled-components';
 
+var container = {
+	width: '95%',
+	textAlign: 'center',
+	margin: '2%'
+};
+
+const Div = styled.div`
+	background-color: #1c2826;
+	width: 95%;
+	margin: 2%;
+	padding: 1%;
+`;
+const Input = styled.input`
+	margin: 2%;
+	padding: 1%;
+	background-color: #d64550;
+	color: #1c2826;
+`;
+const Form = styled.form`
+	width: '95%';
+	text-align: 'center';
+	padding: 2%;
+	margin: 2%;
+	background-color: #1c2826;
+`;
+
+const H2 = styled.h2`
+	padding: 1%;
+	text-align: 'center';
+	background-color: #d64550;
+	color: #1c2826;
+`;
+const H3 = styled.label`
+	padding: 1%;
+	text-align: 'center';
+	margin: 0.25%;
+	background-color: #1c2826;
+	color: #daefb3;
+`;
+const Button = styled.button`
+	padding: 1%;
+	text-align: 'center';
+	margin-left: 25%;
+	margin-bottom: 5%;
+	background-color: #d64550;
+	color: #1c2826;
+	border: 3px solid #daefb3;
+`;
+
 let ContactForm = props => {
 	const [name, setName] = useState('');
 	const [age, setAge] = useState('');
@@ -29,21 +78,22 @@ let ContactForm = props => {
 			});
 	};
 	return (
-		<form onSubmit={handleSubmit}>
-			<div>
-				<label htmlFor="name">Name: </label>
-				<input type="text" name="name" onChange={event => setName(event.target.value)} />
-			</div>
-			<div>
-				<label htmlFor="age">Age: </label>
-				<input type="text" name="age" onChange={event => setAge(event.target.value)} />
-			</div>
-			<div>
-				<label htmlFor="email">email: </label>
-				<input type="text" name="email" onChange={event => setEmail(event.target.value)} />
-			</div>
-			<button type="submit">Submit</button>
-		</form>
+		<Form onSubmit={handleSubmit}>
+			<H2>Enter a Friend:</H2>
+			<Div>
+				<H3 htmlFor="name">Name: </H3>
+				<Input type="text" name="name" onChange={event => setName(event.target.value)} />
+			</Div>
+			<Div>
+				<H3 htmlFor="age"> Age: </H3>
+				<Input type="text" name="age" onChange={event => setAge(event.target.value)} />
+			</Div>
+			<Div>
+				<H3 htmlFor="email">Email: </H3>
+				<Input type="text" name="email" onChange={event => setEmail(event.target.value)} />
+			</Div>
+			<Button type="submit">Submit</Button>
+		</Form>
 	);
 };
 
--- m3/33b1/friends/src/components/DisplayAPI.js
+++ m3/33b1/friends/src/components/DisplayAPI.js
@@ -6,10 +6,41 @@ import { axiosWithAuth } from '../utils/axiosWithAuth';
 import styled from 'styled-components';
 
 var container = {
-	width: '70%',
+	width: '95%',
 	textAlign: 'center',
-	margin: '15%'
+	margin: '2%'
 };
+
+const Div = styled.div`
+	padding: 1%;
+	background-color: #d64550;
+	color: #1c2826;
+`;
+
+const H2 = styled.h2`
+	padding: 1%;
+	background-color: #d64550;
+	color: #1c2826;
+`;
+const H3 = styled.h3`
+	padding: 1%;
+	background-color: #d64550;
+	color: #eef4d4;
+`;
+const H4 = styled.h4`
+	padding: 1%;
+	background-color: #d64550;
+	color: #ea9e8d;
+`;
+const Button = styled.button`
+	padding: 1%;
+	text-align: 'center';
+	margin-left: 25%;
+	margin-bottom: 5%;
+	background-color: #1c2826;
+	color: #d64550;
+	border: 3px solid #daefb3;
+`;
 
 const DisplayAPI = props => {
 	const [id, setID] = useState('');
@@ -27,14 +58,13 @@ const DisplayAPI = props => {
 	};
 
 	return (
-		<div>
-			{console.log('friend = ' + props.name)}
-			<div style={container}>
-				<h2>Name: {props.name}</h2>
-				<h3>Age: {props.age}</h3>
-				<h3>Email: {props.email}</h3>
+		<Div>
+			<Div style={container}>
+				<H2>Name: {props.name}</H2>
+				<H3>Age: {props.age}</H3>
+				<H4>Email: {props.email}</H4>
 				<form onSubmit={handleSubmit}>
-					<button
+					<Button
 						type="submit"
 						name={props.id}
 						id={`${props.id}`}
@@ -42,10 +72,10 @@ const DisplayAPI = props => {
 						onClick={event => setID(event.target.id)}
 					>
 						Delete {props.name}
-					</button>
+					</Button>
 				</form>
-			</div>
-		</div>
+			</Div>
+		</Div>
 	);
 };
 
