02f32e67a0783c7b7ed019bcc6e24c9408954004
Erica L. Ingram <inquiries@aquoco.co>
2020-02-10 11:16:15 -0800

    added delete hub

--- m4/41a1/index.js
+++ m4/41a1/index.js
@@ -5,6 +5,11 @@ const express = require('express'); // CommonJS Modules
 const Hubs = require('./data/hubs-model.js');
 const server = express();
 
+// middleware way to teach express how to do other things that it can't
+// out of the box
+
+server.use(express.json());
+
 server.get('/', (req, res) => {
 	res.json({ hello: 'Web26' });
 	res.status(200);
@@ -16,6 +21,32 @@ server.get('/api/hubs', (req, res) => {
 		.then(hubs => {
 			console.log(hubs);
 			res.status(200).json(hubs);
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).json({ errorMessage: 'oops' });
+		});
+});
+
+// add hub
+server.post('/api/hubs', (req, res) => {
+	//axios post
+	const hubInfo = req.body;
+	Hubs.add(hubInfo)
+		.then(hub => {
+			res.status(201).json(hub);
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).json({ errorMessage: 'oops' });
+		});
+});
+
+// delete hub
+server.delete(`/api/hubs/:id`, (req, res) => {
+	Hubs.remove(req.params.id)
+		.then(removed => {
+			res.status(200).json(removed);
 		})
 		.catch(err => {
 			console.log(err);
