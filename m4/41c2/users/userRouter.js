b16eb310fafb7163e05a0ead1517c678f4698675
Erica L. Ingram <inquiries@aquoco.co>
2020-02-12 16:18:08 -0800

    api gets/posts done

--- m4/41c2/users/userRouter.js
+++ m4/41c2/users/userRouter.js
@@ -12,17 +12,52 @@ router.post('/', validateUser, (req, res) => {
 			res.status(200).json(user);
 		})
 		.catch(error => {
-			res.status(500).json({ errorMessage: 'No user added', error });
+			res.status(500).json({ errorMessage: 'Error adding user.', error });
 		});
 });
 
-router.post('/:id/posts', (req, res) => {});
+router.post('/:id/posts', validateUser, validatePost, (req, res) => {
+	const userID = req.params.id;
+	const bodyText = req.body.text;
+	posts
+		.insert({ user_id: userID, text: bodyText })
+		.then(post => {
+			res.status(200).json({ message: post });
+		})
+		.catch(error => {
+			res.status(500).json({ errorMessage: 'Error adding post.', error });
+		});
+});
 
-router.get('/', (req, res) => {});
+router.get('/', (req, res) => {
+	users
+		.get()
+		.then(users => {
+			res.status(200).json(users);
+		})
+		.catch(error => {
+			res.status(500).json({ errorMessage: 'Error retrieving users.', error });
+		});
+});
 
-router.get('/:id', (req, res) => {});
+router.get('/:id', validateUserId, (req, res) => {
+	const reqUser = req.user;
+	res.status(200).json(reqUser);
+});
 
-router.get('/:id/posts', (req, res) => {});
+router.get('/:id/posts', validateUserId, (req, res) => {
+	const userID = req.user.id;
+	users
+		.getUserPosts(userID)
+		.then(posts => {
+			if (posts.length > 0) {
+				res.status(200).json(posts);
+			}
+		})
+		.catch(error => {
+			res.status(500).json({ errorMessage: 'Error retrieving posts.' });
+		});
+});
 
 router.delete('/:id', (req, res) => {});
 
@@ -44,14 +79,14 @@ function validateUserId(req, res, next) {
 			}
 		})
 		.catch(error => {
-			res.status(500).json({ errorMessage: 'ID required', error });
+			res.status(500).json({ errorMessage: 'missing user ID', error });
 		});
 }
 
 function validateUser(req, res, next) {
 	const name = req.body.name;
 	if (!name) {
-		res.status(404).json({ errorMessage: 'missing user data' });
+		res.status(404).json({ errorMessage: "missing user's name" });
 	} else {
 		req.user = name;
 		next();
@@ -61,7 +96,7 @@ function validateUser(req, res, next) {
 function validatePost(req, res, next) {
 	const post = (req.body.text, req.params.id);
 	if (!req.body.text) {
-		res.status(404).json({ errorMessage: 'missing post data' });
+		res.status(404).json({ errorMessage: 'missing post text' });
 	} else {
 		req.text = post;
 		next();
