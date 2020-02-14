31ded2d6f79cdb4b2a5529b5fd898a47098997ba
Erica L. Ingram <inquiries@aquoco.co>
2020-02-14 09:18:15 -0800

    put request done

--- m4/41e1/routers/projectRouter.js
+++ m4/41e1/routers/projectRouter.js
@@ -41,8 +41,50 @@ router.get('/:id', (req, res) => {
 
 // update one project
 
+router.put('/:id', (req, res) => {
+	const projectID = req.params.id;
+	const projectName = req.params.name;
+	const projectDescription = req.params.description;
+	posts
+		.update(projectID, { projectName, projectDescription })
+		.then(project => {
+			if (project) {
+				res.status(200).json(project);
+			} else {
+				res.status(404).json({ errorMessage: 'No project found with that I.D.' });
+			}
+		})
+		.catch(error => {
+			res.status(500).json({ errorMessage: 'Project not updated.', error });
+		});
+});
+
 // delete one project
 
+router.delete('/:id', (req, res) => {
+	const projectID = req.params.id;
+	posts
+		.remove(projectID)
+		.then(() => {
+			res.status(200).json({ message: `Post ${projectID} was deleted` });
+		})
+		.catch(error => {
+			res.status(500).json({ errorMessage: `Post ${projectID} not deleted.`, error });
+		});
+});
 // create one project
+
+router.post('/:id/posts', (req, res) => {
+	const projectID = req.params.id;
+	const bodyText = req.body.text;
+	posts
+		.insert({ user_id: projectID, text: bodyText })
+		.then(post => {
+			res.status(200).json({ message: post });
+		})
+		.catch(error => {
+			res.status(500).json({ errorMessage: 'Error adding post.', error });
+		});
+});
 
 module.exports = router;
