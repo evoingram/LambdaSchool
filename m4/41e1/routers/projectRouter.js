3200ea3174e0016d3a953aeb563985e1205bf0fd
Erica L. Ingram <inquiries@aquoco.co>
2020-02-14 09:13:39 -0800

    both gets done

--- m4/41e1/routers/projectRouter.js
+++ m4/41e1/routers/projectRouter.js
@@ -21,8 +21,23 @@ router.get('/', (req, res) => {
 			res.status(500).json({ errorMessage: 'The project information could not be retrieved.' });
 		});
 });
-
 //get one project
+router.get('/:id', (req, res) => {
+	const projectID = req.params.id;
+	projects
+		.get(projectID)
+		.then(project => {
+			if (project) {
+				res.status(200).json(project);
+			} else {
+				res.status(404).json({ errorMessage: 'No project found with that I.D.' });
+			}
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).json({ errorMessage: 'No project could be retrieved.' });
+		});
+});
 
 // update one project
 
