0ce22f5cd821ed7f221eed47a091f4972f01e4b6
Erica L. Ingram <inquiries@aquoco.co>
2020-02-18 14:33:56 -0800

    mvp + 2 stretches met

--- m4/42b2/routers/carRouter.js
+++ m4/42b2/routers/carRouter.js
@@ -47,4 +47,55 @@ router.post('/', (req, res) => {
 		});
 });
 
+// update one car
+
+router.put('/:id', (req, res) => {
+	const carID = req.params.id;
+	const car = req.body;
+	const carVIN = req.body.VIN;
+	const carMake = req.body.make;
+	const carModel = req.body.model;
+	const carTransmissionType = req.body.transmissionType;
+	const carTitleStatus = req.body.titleStatus;
+	const carMileage = req.body.mileage;
+	db('cars')
+		.where({ id: carID })
+		.update(carID, {
+			VIN: carVIN,
+			make: carMake,
+			model: carModel,
+			transmissionType: carTransmissionType,
+			titleStatus: carTitleStatus,
+			mileage: carMileage
+		})
+		.then(car => {
+			if (car) {
+				res.status(200).json(car);
+			} else {
+				res.status(404).json({ errorMessage: 'No car found with that I.D.' });
+			}
+		})
+		.catch(error => {
+			res.status(500).json({ errorMessage: 'car not updated.', error });
+		});
+});
+
+// delete one car
+
+router.delete('/:id', (req, res) => {
+	const carID = req.params.id;
+	db('cars')
+		.delete(carID)
+		.where({ id: carID })
+		.then(car => {
+			if (carID) {
+				res.status(200).json({ message: `car ${carID} was deleted` });
+			} else {
+				res.status(404).json({ errorMessage: 'No car found with that I.D.' });
+			}
+		})
+		.catch(error => {
+			res.status(500).json({ errorMessage: `car ${carID} not deleted.`, error });
+		});
+});
 module.exports = router;
