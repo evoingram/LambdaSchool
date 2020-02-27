bb984fce15ccee03a0d1c99b7343fb7e345977a3
Erica L. Ingram <inquiries@aquoco.co>
2020-02-27 14:54:07 -0800

    students/cohorts routers done

--- m4/43d2/cohorts/router.js
+++ m4/43d2/cohorts/router.js
@@ -1,17 +1,91 @@
-const Cohorts = require("./model.js");
+const Cohorts = require('./model.js');
 
-const router = require("express").Router();
+const router = require('express').Router();
 
-router.get("/", (req, res) => {
-  Cohorts.all()
-    .then(cohorts => {
-      res.status(200).json(cohorts);
-    })
-    .catch(({ name, message, stack, code }) => {
-      console.log({ name, message, stack, code });
+/*
+router.get('/', (req, res) => {
+	Cohorts.all()
+		.then(cohorts => {
+			res.status(200).json(cohorts);
+		})
+		.catch(({ name, message, stack, code }) => {
+			console.log({ name, message, stack, code });
 
-      res.status(500).json({ name, message, stack, code });
-    });
+			res.status(500).json({ name, message, stack, code });
+		});
+});
+*/
+
+// get list of cohorts
+router.get('/cohorts', (req, res) => {
+	res.status(201).json({ router: 'cohorts' });
+});
+
+// get one cohort
+router.get('/cohorts/:cohortsid', (req, res) => {
+	const id = req.params.cohortsid;
+
+	Cohorts.getCohort(id)
+		.then(cohort => {
+			if (cohort) {
+				res.json(cohort);
+			} else {
+				res.status(404).json({ message: 'Could not find cohort with given id.' });
+			}
+		})
+		.catch(err => {
+			res.status(500).json({ message: 'Failed to get cohort' });
+		});
+});
+
+// create cohort
+router.post('/cohorts', (req, res) => {
+	const resourceData = req.body;
+
+	Cohorts.addCohort(resourceData)
+		.then(cohort => {
+			res.status(201).json(cohort);
+		})
+		.catch(err => {
+			res.status(500).json({ message: 'Failed to create new cohort' });
+		});
+});
+
+//update cohort
+
+router.put('/cohorts/:cohortsid', (req, res) => {
+	const cohortid = req.params.cohortid;
+	const cohortName = req.body.cohortname;
+	const updatedCohort = { cohortid: cohortid, cohortname: cohortName, cohortdescription: cohortDescription };
+
+	Cohorts.updateCohort(updatedCohort, cohortid)
+		.then(cohort => {
+			if (cohort) {
+				res.json(cohort);
+			} else {
+				res.status(404).json({ message: 'Could not find cohort with given id' });
+			}
+		})
+		.catch(err => {
+			res.status(500).json({ message: 'Failed to update cohort' });
+		});
+});
+
+// delete cohort
+router.delete('/cohorts/:cohortid', (req, res) => {
+	const id = req.params.cohortid;
+
+	Projects.removeCohort(id)
+		.then(deleted => {
+			if (deleted) {
+				res.json({ removed: deleted });
+			} else {
+				res.status(404).json({ message: 'Could not find cohort with given id' });
+			}
+		})
+		.catch(err => {
+			res.status(500).json({ message: 'Failed to delete cohort' });
+		});
 });
 
 module.exports = router;
--- m4/43d2/students/router.js
+++ m4/43d2/students/router.js
@@ -1,7 +1,85 @@
-const router = require("express").Router();
+const router = require('express').Router();
 
-router.get("/", (req, res) => {
-  res.json({ router: "students" });
+const Students = require('./model.js');
+
+router.get('/', (req, res) => {
+	res.json({ router: 'students' });
+});
+
+/*
+// get list of students
+router.get('/students', (req, res) => {
+	res.status(201).json({ router: 'students' });
+});
+*/
+
+// get one student
+router.get('/students/:studentid', (req, res) => {
+	const id = req.params.studentid;
+
+	Students.getStudent(id)
+		.then(student => {
+			if (student) {
+				res.json(student);
+			} else {
+				res.status(404).json({ message: 'Could not find student with given id.' });
+			}
+		})
+		.catch(err => {
+			res.status(500).json({ message: 'Failed to get tasks' });
+		});
+});
+
+// create student
+
+router.post('/students', (req, res) => {
+	const resourceData = req.body;
+
+	Students.addStudent(resourceData)
+		.then(student => {
+			res.status(201).json(student);
+		})
+		.catch(err => {
+			res.status(500).json({ message: 'Failed to create new student' });
+		});
+});
+
+//update student
+
+router.put('/students/:studentsid', (req, res) => {
+	const studentsid = req.params.studentsid;
+	const studentName = req.body.name;
+	const cohortsid = req.body.cohortsid;
+	const updatedStudent = { studentsid: studentsid, name: studentName, cohortsid: cohortsid };
+
+	Students.updateStudent(updatedStudent, studentid)
+		.then(student => {
+			if (student) {
+				res.json(student);
+			} else {
+				res.status(404).json({ message: 'Could not find student with given id' });
+			}
+		})
+		.catch(err => {
+			res.status(500).json({ message: 'Failed to update student' });
+		});
+});
+
+// delete student
+router.delete('/students/:studentsid', (req, res) => {
+	const id = req.params.studentid;
+
+	Students.removeStudent(id)
+		.then(deleted => {
+			if (deleted) {
+				res.json({ removed: deleted });
+			} else {
+				res.status(404).json({ message: 'Could not find student with given id' });
+			}
+		})
+		.catch(err => {
+			res.status(500).json({ message: 'Failed to delete student' });
+		});
 });
 
 module.exports = router;
