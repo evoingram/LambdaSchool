06793e62f4a48e05f35065b5f9ccb6740dd187b8
Erica L. Ingram <inquiries@aquoco.co>
2020-04-11 04:01:56 -0700

    found and added missing files

---
+++ m5/52d1/oauth/oauth2 - initial/src/main/java/com/lambdaschool/authenticatedusers/repository/QuoteRepository.java
@@ -0,0 +1,8 @@
+package com.lambdaschool.authenticatedusers.repository;
+
+import com.lambdaschool.authenticatedusers.model.Quote;
+import org.springframework.data.repository.CrudRepository;
+
+public interface QuoteRepository extends CrudRepository<Quote, Long>
+{
+}
