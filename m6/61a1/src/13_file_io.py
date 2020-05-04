b7e9e0b620cefb12b99c3ef4efffd4e9eea6bbbf
Erica L. Ingram <inquiries@aquoco.co>
2020-05-04 05:47:34 -0700

    step 13 done

--- m6/61a1/src/13_file_io.py
+++ m6/61a1/src/13_file_io.py
@@ -9,11 +9,21 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
-# YOUR CODE HERE
+def fileIORead():
+    with open('foo.txt') as f:
+        read_data = f.read()
+        print(read_data)
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
-# YOUR CODE HERE
+def fileIOWrite():
+    string1 = "arbitrary 1"
+    string2 = "content 2"
+    string3 = "to the file 3"
+    f = open("bar.txt", "w+")
+    f.write(string1)
+    f.write(string2)
+    f.write(string3)
