ed8b3f825ff2d318441879e30f2d3663282cb891
Erica L. Ingram <inquiries@aquoco.co>
2020-05-08 14:27:58 -0700

    62a1

--- m6/62a1/binary_search_tree/binary_search_tree.py
+++ m6/62a1/binary_search_tree/binary_search_tree.py
@@ -12,21 +12,38 @@ class BinarySearchTree:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        
+        if value < self.value:
+               
+            if not self.left:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
+
+        elif value >= self.value:
+
+            if not self.right:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
+
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
         pass
 
+
     # Return the maximum value found in the tree
     def get_max(self):
         pass
+
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
         pass
+
 
     # DAY 2 Project -----------------------
 
@@ -35,22 +52,27 @@ class BinarySearchTree:
     def in_order_print(self, node):
         pass
 
+
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self, node):
         pass
+
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self, node):
         pass
 
+
     # STRETCH Goals -------------------------
     # Note: Research may be required
+
 
     # Print Pre-order recursive DFT
     def pre_order_dft(self, node):
         pass
+
 
     # Print Post-order recursive DFT
     def post_order_dft(self, node):
