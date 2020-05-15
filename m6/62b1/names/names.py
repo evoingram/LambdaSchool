22a5b6f37738ef9dbfb6aafbf07c01ec1906972d
Erica L. Ingram <inquiries@aquoco.co>
2020-05-14 17:56:31 -0700

    names/buffer done

--- m6/62b1/names/names.py
+++ m6/62b1/names/names.py
@@ -1,9 +1,11 @@
+from binary_search_tree import BinarySearchTree
 import time
 
 # MVP: 
 # Navigate into the `names` directory. Here you will find two text files containing 10, 000 names each, along with a program `names.py` that compares the two files and prints out duplicate name entries.
 # Try running the code with `python3 names.py`. Be patient because it might take a while: approximately six seconds on my laptop.
 # What is the runtime complexity of this code?
+# runtime: 5.663139343261719 seconds
 # Six seconds is an eternity so you've been tasked with speeding up the code. Can you get the runtime to under a second? Under one hundredth of a second?
 # You may not use the built in Python list, set, or dictionary in your solution for this problem.  However, you can and should use the provided `duplicates` list to return your solution.*
 # (Hint: You might try importing a data structure you built during the week)
@@ -32,10 +34,26 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
+'''
 for name_1 in names_1:
     for name_2 in names_2:
         if name_1 == name_2:
             duplicates.append(name_1)
+'''
+
+
+class BSTNames:
+    def __init__(self, value=None):
+        self.node_value = value
+        self.node_left = None
+        self.node_right = None
+
+    def insert(self, value):
+        BinarySearchTree.insert(self, value)
+
+    def contains(self, target):
+        BinarySearchTree.contains(self, target)
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
@@ -45,3 +63,6 @@ print (f"runtime: {end_time - start_time} seconds")
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+# os.chdir("E:\\projects\\LambdaSchool\\m6\\62b1\\names")
+# exec(open("names.py").read())
