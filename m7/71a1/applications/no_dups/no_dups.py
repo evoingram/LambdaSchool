f620e004e082cae4ef5c536cf369b975d10037a0
Erica L. Ingram <inquiries@aquoco.co>
2020-06-02 07:24:31 -0700

    day 3, #8 done

--- m7/71a1/applications/no_dups/no_dups.py
+++ m7/71a1/applications/no_dups/no_dups.py
@@ -1,6 +1,20 @@
 def no_dups(s):
-    # Your code here
+    # Input: a string of words separated by spaces. Only the letters a-z are utilized.
+    # Output: the string in the same order, but with subsequent duplicate words removed.
+    # There must be no extra spaces at the end of your returned string.
+    # The solution must be O(n).
 
+    word_array = s.split(" ")
+    print(word_array)
+    cache = {}
+    no_duplicates = []
+    for word in word_array:
+        if word not in cache:
+            cache[word] = 1
+            no_duplicates.append(word)
+    final_string = ' '.join(no_duplicates)
+    print(final_string)
+    return final_string
 
 
 if __name__ == "__main__":
