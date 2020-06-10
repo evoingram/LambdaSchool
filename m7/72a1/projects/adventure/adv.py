c77cfa3bf3322926720ba0aec0ce8f8a027172fe
Erica L. Ingram <inquiries@aquoco.co>
2020-06-10 10:54:40 -0700

    day 4, pseudocode/plan tentatively set out

--- m7/72a1/projects/adventure/adv.py
+++ m7/72a1/projects/adventure/adv.py
@@ -1,13 +1,13 @@
 from room import Room
 from player import Player
 from world import World
+from util import Stack, Queue, Graph.
 
 import random
 from ast import literal_eval
 
 # Load world
 world = World()
-
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
@@ -29,6 +29,45 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+    
+# Instead of searching for a target vertex, searching for an exit with a `'?'` as the value.
+# If an exit has been explored, you can put it in your BFS queue like normal.
+
+# BFS will return the path as a list of room IDs.
+# You will need to convert this to a list of n/s/e/w directions before you can add it to your traversal path.
+# If all paths have been explored, you're done!
+
+
+# Explore rooms to get all traversable paths
+# THEN calculate shortest path
+
+
+# 0: [(3, 5), {'n': 1}],
+# room number: [(x, y coordinates), {direction: rooms it leads to}]
+# commands:  player.current_room.id, player.current_room.get_exits() and player.travel(direction)
+
+
+def unexplored_room():
+    pass
+    # get current player room (player.current_room.id, current_player_room)
+    # get available directions by searching for '?' (player.current_room.get_exits(), get_room_in_direction(), current_room_exits)
+    # select available direction (current_room_exits)
+    # log that direction to your graph (traversed_paths, current_path)
+    # go that direction (player.travel(direction), next_room)
+    # if room has no unexplored paths, walk back to nearest room w/ one (separate function maybe, current_path/next_path)
+    # then loop
+
+
+
+# organize above steps into separate functions:
+    # unexplored room function
+    # room with no unexplored paths function
+    # maybe function to get directions and next-rooms from a current room
+
+
+
+
+# find the path to the shortest unexplored room by using a breadth-first search for a room with a `'?'` for an exit.
 
 
 # TRAVERSAL TEST
